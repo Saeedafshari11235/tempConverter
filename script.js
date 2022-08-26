97423beab28a0e26395e7de9b8b2bcef1ae62b26
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-26 17:30:27 +0430

    adding change event

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ clear.addEventListener("click",function(){
     inp.value = ""
 })
 convert.addEventListener("click",function(){
-
+    
 })
 change.addEventListener("click",function(){
-
+    flag++
+    if(flag%2==0){
+        document.title = "°F to °C converter"
+        ttl.innerHTML = "°F to °C"
+    }
+    else{
+        document.title =" °C to °F converter"
+        ttl.innerHTML = "°C to °F"
+    }
 })
