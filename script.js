a50d7a908db5dbe2b9ba35c31b89d119bb577251
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-26 17:20:44 +0430

    adding DOM vars

---
+++ script.js
@@ -0,0 +1,5 @@
+let inp = document.querySelector("#temps")
+let clear = document.querySelector("#clear")
+let change = document.querySelector("#change")
+let convert = document.querySelector("#convert")
+let flag = 1
