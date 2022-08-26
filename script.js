433b4ccca1ee02fa5225f1fc6670ecf4baef0a3c
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-26 17:29:45 +0430

    adding event listener

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let inp = document.querySelector("#temps")
+let inp = document.querySelector("#temp")
+let ttl = document.querySelector("legend")
 let clear = document.querySelector("#clear")
 let change = document.querySelector("#change")
 let convert = document.querySelector("#convert")
 let flag = 1
+clear.addEventListener("click",function(){
+
+})
+convert.addEventListener("click",function(){
+
+})
+change.addEventListener("click",function(){
+
+})
